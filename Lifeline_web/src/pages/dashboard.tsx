003d2cf808bdd480e10c62ebeb2221f5836d9863
cess2c <tumamposfrances@gmail.com>
2025-12-28 20:57:03 +0800

    added WebSocket

--- Lifeline_web/src/pages/dashboard.tsx
+++ Lifeline_web/src/pages/dashboard.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/dashboard.css'
-import { useDashboard } from "../components/useDashboard.tsx";
+import { useDashboard } from "../components/useDashboard";
 
 
 function Dashboard() {
-  const [count, setCount] = useState(0)
   const { 
-    handleLogout, 
+    handleLogout,
+    handleSOS,
+    message,
+    time,
   } = useDashboard();
   return (
     <main className='dashboard'>
     <section>
       <h1>Dashboard</h1>
+      <h3>Server Time: {time}</h3>
+      <p>Server Message: {message}</p>
+      <button onClick={handleSOS}>
+        sos
+      </button>
       <button onClick={handleLogout}>
         logout
       </button>
