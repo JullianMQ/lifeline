1112295d8b7d619eac2254ee4e7b6c6f04da2891
Jullian Quiambao <jullianq.dev@gmail.com>
2026-01-09 18:24:13 +0800

    update user and database configuration. Made custom route for updating user information

--- Lifeline_Backend/src/lib/db.ts
+++ Lifeline_Backend/src/lib/db.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import path = require('path');
 
 const caPath = path.resolve("./ca.pem")
-export const dbPool = new Pool({
+const production = new Pool({
     connectionString: process.env.AIVEN_DB_URL?.replace('&sslmode=require', ''),
     max: 20,
     idleTimeoutMillis: 30000,
@@ -12,6 +12,14 @@ export const dbPool = new Pool({
         rejectUnauthorized: false,
         ca: fs.readFileSync(caPath, "utf8")
     }
-});
+})
 
+const development = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+
+export const dbPool = process.env.NODE_ENV === 'production' ? production : development;
 export default dbPool;
