2c5eb800a18e86685b79d4d0ae42beff07bdb7eb
Chester Cruz <chestercomptia@gmail.com>
2026-01-12 23:23:22 +0800

    added .env.example

--- LifeLine_App/lib/api/config.ts
+++ LifeLine_App/lib/api/config.ts
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = "http://10.0.2.2:3000";
 // export const API_BASE_URL = "http://192.168.100.185:3000";
-export const API_BASE_URL = "https://api.lifeline-help.me";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 
