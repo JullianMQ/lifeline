c975f842d7f61a3fa808672e0fb043a00d345eeb
Chester Cruz <chestercomptia@gmail.com>
2026-01-18 15:12:47 +0800

    remove unused files/imports

--- LifeLine_App/app/_layout.tsx
+++ LifeLine_App/app/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import "./globals.css";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
-import "../lib/tasks/background_sensors";
 import { SensorProvider } from "@/lib/context/sensor_context";
 
 GoogleSignin.configure({
