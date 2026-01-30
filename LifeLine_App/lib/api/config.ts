8f95a0ed3e55b6079acdd8220b84b48767d82e84
Chester Cruz <chestercomptia@gmail.com>
2026-01-30 20:12:14 +0800

    Add SOS media capture

--- LifeLine_App/lib/api/config.ts
+++ LifeLine_App/lib/api/config.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL ?? "http://localhost:3000";
-export const WS_BASE_URL = process.env.EXPO_PUBLIC_WS_BASE_URL ?? "ws://localhost:3000";
+export const API_BASE_URL = "http://192.168.100.185:3000";
+export const WS_BASE_URL = "ws://192.168.100.185:3000";
 
 
 //process.env.EXPO_PUBLIC_API_BASE_URL ??
